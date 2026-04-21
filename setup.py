729876bf0a8561323746f3d384f4261f0d3e750c
Abhay2-singh <rajawatabhaysingh05@gmail.com>
2026-04-21 23:22:48 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.0.1",
+    author="Abhay",
+    author_email="[rajawatabhaysingh05@gmail.com]",
+    packages=find_packages(),
+    install_requires=[],
+)
